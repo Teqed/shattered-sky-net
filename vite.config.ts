3765e2df10721a1b33e4021dcca6869c0cc152aa
Timothy Quilling <teqed.arken@gmail.com>
2023-01-11 15:52:12 -0500

    reinit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import importToCDN, { autoComplete } from 'vite-plugin-cdn-import'
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  plugins: [
+    vue(),
+    vueJsx(),
     importToCDN({
-        modules: [
-            autoComplete('vue'), // vue2 use autoComplete('vue2')
-            autoComplete('@vueuse/shared'),
-            autoComplete('@vueuse/core')
-        ],
+      modules: [
+        autoComplete("vue"),
+        {
+          name: "vue-router",
+          var: "VueRouter",
+          path: "dist/vue-router.esm-browser.js",
+        },
+        {
+          name: "pinia",
+          var: "Pinia",
+          path: "dist/pinia.esm-browser.js",
+        },
+      ],
     }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
