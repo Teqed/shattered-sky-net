ec378ef479fcfccf50f30108ae24bf7dcc03c10c
Timothy Quilling <teqed.arken@gmail.com>
2023-02-18 14:36:36 -0500

    change default port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [
 	  {
 		name: 'shattered-sky-net',
-		port: '3000',
+		port: '3344',
 		exec_mode: 'cluster',
 		instances: 'max',
 		script: './.output/server/index.mjs'
