c8cf23ae93b4f68ce784ba23558c009eecad9660
Timothy Quilling <teqed.arken@gmail.com>
2023-02-19 02:11:10 -0500

    import external packages from cdn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// import { fileURLToPath, URL } from "node:url";
-// import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
+import { fileURLToPath, URL } from "node:url";
+import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
 import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
@@ -24,37 +24,37 @@ export default defineNuxtConfig({
 	},
 	vite: {		
 	plugins: [
-		// importToCDN({
-		//   modules: [
-		// 	autoComplete("vue"),
-		// 	{
-		// 	  name: "vue-router",
-		// 	  var: "VueRouter",
-		// 	  path: "dist/vue-router.global.prod.js",
-		// 	},
-		//   ],
-		// }),
+		importToCDN({
+		  modules: [
+			autoComplete("vue"),
+			{
+			  name: "vue-router",
+			  var: "VueRouter",
+			  path: "dist/vue-router.global.prod.js",
+			},
+		  ],
+		}),
 	  ],
 	  resolve: {
-		// alias: {
-		//   "@": fileURLToPath(new URL("./", import.meta.url)),
-		// },
+		alias: {
+		  "@": fileURLToPath(new URL("./", import.meta.url)),
+		},
 	  },
 	},
 	app: {
 		head: {
 			link: [
 			],
-	// 		"script": [
-	// 			// Import Vue from CDN URL
-	// 			{
-	// 				"src": "https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.prod.js",
-	// 			},
-	// 			// Import Vue Router from CDN URL
-	// 			{
-	// 				"src": "https://cdn.jsdelivr.net/npm/vue-router@4.1.6/dist/vue-router.global.prod.js",
-	// 			},
-	// 		],
+			"script": [
+				// Import Vue from CDN URL
+				{
+					"src": "https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.prod.js",
+				},
+				// Import Vue Router from CDN URL
+				{
+					"src": "https://cdn.jsdelivr.net/npm/vue-router@4.1.6/dist/vue-router.global.prod.js",
+				},
+			],
 		  },
 	},
 });
