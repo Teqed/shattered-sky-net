07d52d297769c43cd3f93c0b66916dea30d70649
Timothy Quilling <teqed.arken@gmail.com>
2023-02-19 02:44:57 -0500

    revert regression

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { fileURLToPath, URL } from "node:url";
-import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
-import { defineNuxtConfig } from 'nuxt/config'
+// import { fileURLToPath, URL } from "node:url";
+// import { autoComplete, Plugin as importToCDN } from "vite-plugin-cdn-import";
+// import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
 	// webpack:{
@@ -24,35 +24,36 @@ export default defineNuxtConfig({
 	},
 	vite: {		
 	plugins: [
-		importToCDN({
-		  modules: [
-			autoComplete("vue"),
-			{
-			  name: "vue-router",
-			  var: "VueRouter",
-			  path: "dist/vue-router.global.prod.js",
-			},
-		  ],
-		}),
+		// importToCDN({
+		//   modules: [
+		// 	autoComplete("vue"),
+		// 	{
+		// 	  name: "vue-router",
+		// 	  var: "VueRouter",
+		// 	  path: "dist/vue-router.global.prod.js",
+		// 	},
+		//   ],
+		// }),
 	  ],
 	  resolve: {
-		alias: {
-		  "@": fileURLToPath(new URL("./", import.meta.url)),
-		},
+		// alias: {
+		//   "@": fileURLToPath(new URL("./", import.meta.url)),
+		// },
 	  },
 	},
 	app: {
 		head: {
 			link: [
+
 			],
-			"script": [
+			script: [
 				// Import Vue from CDN URL
 				{
-					"src": "https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.prod.js",
+					// "src": "https://cdn.jsdelivr.net/npm/vue@3.2.45/dist/vue.global.prod.js",
 				},
 				// Import Vue Router from CDN URL
 				{
-					"src": "https://cdn.jsdelivr.net/npm/vue-router@4.1.6/dist/vue-router.global.prod.js",
+					// "src": "https://cdn.jsdelivr.net/npm/vue-router@4.1.6/dist/vue-router.global.prod.js",
 				},
 			],
 		  },
