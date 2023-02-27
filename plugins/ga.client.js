2141326bebb719ff9791483471c7ee667d45cf59
Timothy Quilling <teqed.arken@gmail.com>
2023-02-27 16:28:14 -0500

    cleanup comments

--- plugins/ga.client.js
+++ plugins/ga.client.js
@@ -2,31 +2,17 @@
 
 import VueGtag from 'vue-gtag';
 
-// export default ({ isDev, app }) => {
-// 	if (!isDev) {
-// 		Vue.use(VueGtag, {
-// 			config: { id: 'G-2FGMFZNKDY' }
-// 		},
-// 		app.router);
-// 	} else {
-// 		console.log('Skipping GA in development')
-// 	}
-// }
-
-// export default defineNuxtPlugin((nuxtApp) => {
-// 	if (!nuxtApp.vueApp.isDev) {
-// 	useNuxtApp(VueGtag, {
-// 		config: { id: 'G-2FGMFZNKDY' }
-// 	},
-// 	nuxtApp.vueApp.router);
-// }
-// })
-
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.use(VueGtag, {
 		config: {
 			id: 'G-2FGMFZNKDY'
-		}
-	}, nuxtApp.vueApp.router)
-	// trackRouter(useRouter())
+		},
+		onReady: () => {
+			console.log('Google Analytics is ready');
+		},
+		onError: () => {
+			// console.log('An error occurred while sending data to Google Analytics, but it is not critical:');
+		},
+		deferScriptLoad: true,
+	}, nuxtApp.vueApp.router);
 })
