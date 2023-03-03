07f6c70bcf7b17c076299ec06152558435665ba8
Timothy Quilling <teqed.arken@gmail.com>
2023-03-02 20:29:14 -0500

    add toasts

---
+++ plugins/toast.client.js
@@ -0,0 +1,13 @@
+// plugins/toast.client.js
+
+import VueNotificationList from '@dafcoe/vue-notification';
+import '@dafcoe/vue-notification/dist/vue-notification.css';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueNotificationList, {
+    position: 'bottom-center',
+    duration: 5000,
+    max: 5,
+    appearance: 'dark',
+  });
+});
