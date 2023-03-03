9a58ff7ecb61c48f725afd19d5b61718367bd19a
Timothy Quilling <teqed.arken@gmail.com>
2023-03-03 16:00:03 -0500

    add page

---
+++ pages/three/03.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+<script lang="ts">
+import Three03 from '@/components/Three03.vue';
+
+export default {
+	name: 'Page03',
+	components: {
+		Three03,
+	},
+};
+</script>
+<template>
+  <div>
+    <Three03 />
+  </div>
+</template>
+<style scoped></style>
