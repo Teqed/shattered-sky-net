63adc0cb24fb6f65eac6c39a59ff6534a1409be6
Timothy Quilling <teqed.arken@gmail.com>
2023-03-15 03:42:12 -0400

    add logging

--- server/api/openai.ts
+++ server/api/openai.ts
@@ -1,4 +1,31 @@
 import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai'
+import * as winston from 'winston'
+
+const logger = winston.createLogger({
+	level: 'info',
+	format: winston.format.json(),
+	defaultMeta: { service: 'user-service' },
+	transports: [
+	//
+	// - Write all logs with importance level of `error` or less to `error.log`
+	// - Write all logs with importance level of `info` or less to `combined.log`
+	//
+		new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+		new winston.transports.File({ filename: 'logs/combined.log' }),
+		// Create a file just for message logging
+		new winston.transports.File({ filename: 'logs/messages.log', level: 'verbose' }),
+	],
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+	logger.add(new winston.transports.Console({
+		format: winston.format.simple(),
+	}));
+}
 
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -9,9 +36,25 @@ const deniedReply: ChatCompletionRequestMessage = {
 	content: 'Your message was deemed potentially unsafe by the automated moderation API and not sent.',
 }
 
+const writeLog = (message: string, question: boolean) => {
+	if (question) {
+		logger.verbose(
+			`Question: ${message}`
+		);
+	} else {
+		logger.verbose(
+			`Answer: ${message}`
+		);
+	}
+};
+
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event)
 	const { messages } = body
+	// Write messages[messages.length - 1].content to a file
+
+	writeLog(messages[messages.length - 1].content, true)
+
 	console.time('moderation')
 	const moderation = await openai.createModeration({
 		input: messages[messages.length - 1].content,
@@ -28,6 +71,10 @@ export default defineEventHandler(async (event) => {
 		});
 		console.timeEnd('conversation')
 		const reply = conversation.data.choices[0].message
+		writeLog(
+			reply?.content ?? 'No reply',
+			false
+		)
 
 		return reply;
 	}
