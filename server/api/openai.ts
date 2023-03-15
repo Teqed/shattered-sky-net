67d434ccdd4a7f4c9cec3990d4b08168fba5fde4
Timothy Quilling <teqed.arken@gmail.com>
2023-03-15 03:10:29 -0400

    add moderation

--- server/api/openai.ts
+++ server/api/openai.ts
@@ -1,29 +1,34 @@
-import { Configuration, OpenAIApi } from 'openai'
+import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
-// });
+const deniedReply: ChatCompletionRequestMessage = {
+	role: 'assistant',
+	content: 'Your message was deemed potentially unsafe by the automated moderation API and not sent.',
+}
 
 export default defineEventHandler(async (event) => {
 	const body = await readBody(event)
 	const { messages } = body
-	// const moderation = await openai.createModeration({
-	// 	input: messages[messages.length - 1].content,
-	// });
-	// if (moderation.data.results[0].flagged === true) {
-	// 	messages.pop()
-	// 	return 'Your message was not sent because it was deemed unsafe by our moderation API.';
-	// } else {
-	const conversation = await openai.createChatCompletion({
-		model: 'gpt-3.5-turbo',
-		messages,
+	console.time('moderation')
+	const moderation = await openai.createModeration({
+		input: messages[messages.length - 1].content,
 	});
-	console.log(conversation)
-	const reply = conversation.data.choices[0].message
+	console.timeEnd('moderation')
+	if (moderation.data.results[0].flagged === true) {
+		messages.pop()
+		return deniedReply;
+	} else {
+		console.time('conversation')
+		const conversation = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages,
+		});
+		console.timeEnd('conversation')
+		const reply = conversation.data.choices[0].message
 
-	return reply;
-	// }
+		return reply;
+	}
 })
