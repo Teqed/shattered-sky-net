a5e021141766bd4c3ab4c433899721427affc95a
Timothy Quilling <teqed.arken@gmail.com>
2023-03-18 13:46:40 -0400

    disable analytics

--- plugins/ga.client.js
+++ plugins/ga.client.js
@@ -1,16 +1,16 @@
 // /plugins/ga.client.js
 
-import VueGtag from 'vue-gtag';
+// import VueGtag from 'vue-gtag';
 
 export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.vueApp.use(VueGtag, {
-		config: {
-			id: 'G-2FGMFZNKDY'
-		},
-		onError: (_error) => {
-			return true;
-		},
-		enabled: false,
-		deferScriptLoad: true,
-	}, nuxtApp.vueApp.router);
+	// nuxtApp.vueApp.use(VueGtag, {
+	// 	config: {
+	// 		id: 'G-2FGMFZNKDY'
+	// 	},
+	// 	onError: (_error) => {
+	// 		return true;
+	// 	},
+	// 	enabled: false,
+	// 	deferScriptLoad: true,
+	// }, nuxtApp.vueApp.router);
 })
