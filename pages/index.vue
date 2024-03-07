f7ea5d09451729f32d7a95eab121d5f58f452987
Timothy Quilling <teqed@shatteredsky.net>
2024-03-07 14:24:54 -0500

    Add organization and breadcrumblist to schema

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,105 @@
 <script setup lang="ts">
+useSchemaOrg([
+  defineBreadcrumb({
+            "@type":"BreadcrumbList",
+            "@id":"https://shatteredsky.net#breadcrumb",
+            "itemListElement": [{
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Home",
+                "item": "https://shatteredsky.net"
+                },{
+                "@type": "ListItem",
+                "position": 2,
+                "name": "Files",
+                "item": "https://shatteredsky.net/files"
+                },{
+                "@type": "ListItem",
+                "position": 3,
+                "name": "Tabletop",
+                "item": "https://shatteredsky.net/tabletop"
+                },{
+                "@type": "ListItem",
+                "position": 4,
+                "name": "Stream",
+                "item": "https://shatteredsky.net/stream"
+                },{
+                "@type": "ListItem",
+                "position": 5,
+                "name": "Social",
+                "item": "https://shatteredsky.net/social"
+                },{
+                "@type": "ListItem",
+                "position": 6,
+                "name": "About",
+                "item": "https://shatteredsky.net/about"
+                }
+            ]
+        }),
+  defineWebPage({
+            "@type": "WebPage",
+            "@id":"https://shatteredsky.net#webpage",
+            "url":"https://shatteredsky.net",
+            "name":"Shattered Sky",
+            "isPartOf":{
+                "@id":"https://shatteredsky.net#website"
+            },
+            "author":{
+                "@id":"https://quilling.dev#person"
+            },
+            "about":{
+                "@id":"https://shatteredsky.net#organization"
+            },
+            "datePublished":"2024-03-06T05:30:13+0000",
+            "dateModified":"2024-03-06T05:30:13+0000",
+            "description":"The hosting platform of Shattered Sky, a digital community.",
+            "breadcrumb":{
+                "@id":"https://shatteredsky.net#breadcrumb"
+            },
+            "inLanguage":"en-US, zh-CN, fr-FR",
+            "potentialAction":[{
+                "@type":"ReadAction",
+                "target":["https://shatteredsky.net"]
+            }]
+        }),
+  defineWebSite({
+            "@type":"WebSite",
+            "@id":"https://shatteredsky.net#website",
+            "url":"https://shatteredsky.net",
+            "name":"Shattered Sky",
+            "description":"Homepage for Shattered Sky",
+            "publisher":{
+                "@id":"https://quilling.dev#person"
+            },
+            "potentialAction":[{
+                "@type":"SearchAction",
+                "target":{
+                    "@type":"EntryPoint",
+                    "urlTemplate":"https://shatteredsky.net/?s={search_term_string}"
+                },
+                "query-input":"required name=search_term_string"
+            }],
+            "inLanguage":"en-US"
+        }),
+  defineOrganization({
+            "@type":"Organization",
+            "@id":"https://shatteredsky.net#organization",
+            "name":"Shattered Sky",
+            "url":"https://shatteredsky.net",
+            "logo":"https://shatteredsky.net/assets/images/logo.png",
+            "founder":{
+                "@type":"Person",
+                "@id":"https://quilling.dev#person"
+            },
+            "member":{
+                "@type":"Person",
+                "@id":"https://quilling.dev#person"
+            },
+            "subjectOf":{
+                "@id":"https://shatteredsky.net#website"
+            }
+        }),
+]);
 useHead({
 	title: 'Shattered Sky',
 	titleTemplate: '%s',
